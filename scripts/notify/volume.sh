594892738fa198a002338a0540daec23fa2f5b32
NBonaparte <98007b33@opayq.com>
2016-11-24 17:49:26 -0800

    Add correct version of volume.sh

--- scripts/notify/volume.sh
+++ scripts/notify/volume.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-LEVEL=$(cat /sys/class/backlight/acpi_video0/actual_brightness)
-OUTPUT=""
-if [ $LEVEL != "0" ]; then
-	OUTPUT+=$(printf '▮ %.0s' $(seq 1 $LEVEL))
-fi
-if [ $(($LEVEL-15)) != "0" ]; then
-	OUTPUT+=$(printf '▯ %.0s' $(seq $LEVEL 14)) #15 brightness levels
-fi
-touch /tmp/brightness_id
-ID=$(cat /tmp/brightness_id)
+case "$1" in
+	high*)
+		amixer sset Master 5%+ -M unmute
+		;;
+	low*)
+		amixer sset Master 5%- -M unmute
+		;;
+	mute*)
+		amixer sset Master toggle
+		;;
+esac
+OUTPUT=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master -M))
+touch /tmp/volume_id
+ID=$(cat /tmp/volume_id)
 if [ $ID -gt "0" ]; then
-	dunstify -p -r $ID "Brightness" "$OUTPUT" > /tmp/brightness_id
+	dunstify -p -r $ID "Volume" "$OUTPUT" > /tmp/volume_id
 else
-	dunstify -p "Brightness" "$OUTPUT" > /tmp/brightness_id
+	dunstify -p "Volume" "$OUTPUT" > /tmp/volume_id
 fi
 #notify-send "Brightness" "$OUTPUT" -t 1
